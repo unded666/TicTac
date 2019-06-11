185d4c602b1728ef35c4bf7c71b8683dec7ed424
Evan Hurwitz <hurwitze@gmail.com>
2019-06-11 16:12:36 +0100

    strategy fleshed out a bit

--- ToeStrats.py
+++ ToeStrats.py
@@ -7,9 +7,29 @@ Created on Tue Jun 11 15:55:34 2019
 
 import random
 
+class toe_strat:
+    
+    s_call = None
+    u_call = None
+    
+    def __init__ (self, stratcall, updatecall):
+        self.s_call = stratcall
+        self.u_call = updatecall
+        
+    def call_strat (self, **kwargs):
+        return self.s_call (**kwargs)
+    
+    def update_strat (self, **kwargs):
+        
+        self.u_call (**kwargs)
+
 def __random_strat (board, moves):
     
     return random.choice (moves)
+
+def __update_random_strat (**args):
+    
+    return Null
 
 def get_strategy (strat) :
     
